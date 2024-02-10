d0702787375419ac027c5aec252055be213f0e20
Greg Goad <ggoadmusic@gmail.com>
2024-02-10 13:12:54 -0500

    Removed debugging comments and added comments for clarity

--- crawl.php
+++ crawl.php
@@ -5,25 +5,52 @@ set_time_limit(0);
 require_once("php_library/postVarSet.php");
 require_once("php_library/sQuote.php");
 
-
+// href is the url you want to crawl.
+PostVarSet('href',$href);
 
 function SchemeAndHost($url){
 	$pu=parse_url($url);
 	
 	return $pu['scheme'].'://'.$pu['host'];
 }
-PostVarSet('href',$href);
 
+// homeUrl could be a constant? Next update
 $homeUrl=SchemeAndHost($href);
 
+// crawlArr is a que for what needs to be crawled. 
+// Here, it is initialized with the $href
+/*a crawlArr object:
+	{
+		href     : string  : req : the endpoint to be crawled
+		parent   : integer : opt : the index of the result that added this endpoint to the crawl que
+		count300 : integer : opt : how many 30x there had been (to prevent circular redirects)
+		noFollow : bool    : opt : if this is set to true, then none of the links on the page are indexed and added to the crawl que,
+		data     : string  : opt : only used in the RELATIVE_ERROR special case
+	}
+*/
 $crawlArr=[['href'=>$href, 'parent'=>null]];
+
+// this will store our crawl results.
+/* a result object:
+	{
+		href            : string : the href that was crawled,
+		responseCode    : string : the resposne code of the request,
+		responseSuccess : bool   : success flag,
+		headers         : string : the response headers,
+		links           : array  : a list of anchor href values on the page
+		data            : string : any extra comments or problems
+	}
+	
+	notice the body isn't saved. Just a ton of data if you did. Trivial to add.
+*/
 $results=[];
-$stagedArr=[];
 
 $max=INF;
 $run=0;
 
+// dom parse
 $dom=new DOMDocument();
+
 function NextCrawl(){
 	global $max;
 	global $run;
@@ -31,30 +58,38 @@ function NextCrawl(){
 	global $results;
 	global $dom;
 	global $homeUrl;
-	global $stagedArr;
 	
+	// This is so you can set a maximum execution count.
 	$run++;
 	if(!$crawlArr || $run >= $max){
 		return false;
 	}
+	
+	// Best practice is to sleep between 0.75 and 1 seconds between requests.
 	sleep(1);
 	
 	
+	// gets the next array member 
 	$findit=array_shift($crawlArr);
 	$url=$findit['href'];
 	
+	// This is a special case from the end of the NextCrawl function, where we can't resolve the next URL... but we still want to maintain a record that the failure happened.
 	if($url === 'RELATIVE ERROR'){
 		$results[]=[
 			'href'=>'RELATIVE ERROR',
-			'data'=>$findit['data']
+			'data'=>$findit['data'],
+			'responseSuccess'=>false
 		];
 		return NextCrawl();
 	}
 	
+	// parse the optional data from the crawl object
 	$parentIndex=$findit['parent'] ?? -1;
 	$noFollow=$findit['noFollow'] ?? false;
 	$count300=$findit['count300'] ?? 0;
 	
+	// blocks circular redirects. 
+	// this is the fast solution. A redirect array with a search might would be better.
 	if($count300 > 5){
 		$results[]=[
 			'href'=>$url,
@@ -63,9 +98,12 @@ function NextCrawl(){
 		];
 	}
 	
+	// check if the endpoint has already been crawled, and if it has it moves to the next crawl.
 	if(array_filter($results, function($r) use ($url){return $r['href'] === $url;})){
 		return NextCrawl();
 	}
+	
+	// run the http request
 	$opts = [
 		"http" => [
 			"method" => "GET",
@@ -75,44 +113,52 @@ function NextCrawl(){
 	$context = stream_context_create($opts);
 	$resp=@file_get_contents($url, false, $context);
 	
+	// checks for a complete failure
 	if($resp === false){
 		$results[]=[
 			'href'=>$url,
 			'responseCode'=>'599',
 			'responseSuccess'=>false,
-			'headers'=>[],
+			'headers'=>'',
 			'links'=>[],
-			'problem'=>'failed file get contents'
+			'data'=>'failed file get contents'
 			
 		];
 		return NextCrawl();
 	}
 	
+	// parses out the response code
 	$respCode=intval(explode(' ',$http_response_header[0])[1]);
 	
-	
+	// gets the potential result index, to pass to any children links
 	$thisIndex=count($results);
 	
+	// construct the result object
 	$resultOb=[
 		'href'=>$url,
 		'responseCode'=>''.$respCode,
 		'responseSuccess'=>$respCode <= 400,
 		'headers'=>$http_response_header,
-		//'body'=>$resp,
 		'links'=>[]
 	];
+	
+	// if a parent exists, pass that information
 	if($parentIndex > -1){
 		$resultOb['parentIndex']=$parentIndex;
 	}
+	
+	// ### THIS IS PASSED BY REFERENCE!
+	// It is done like this so we can go ahead and return on a 400, but also append to $result['links'] upon crawling
 	$results[]=&$resultOb;
+	
 	
 	if($respCode >= 400){
 		return NextCrawl();
 	}
+	
+	// if a redirect, make sure to increment count300
 	if($respCode >= 300){
 		$loc=array_values(array_filter($http_response_header, function($h){return preg_match('/^Location:/i',$h);}));
-		//die(json_encode($loc));,''
-		//if(!$loc){die(json_encode($http_response_header));}
 		array_unshift($crawlArr,[
 			'href'=>trim(preg_replace('/^location:/i','',$loc[0])),
 			'parent'=>$thisIndex,
@@ -123,34 +169,43 @@ function NextCrawl(){
 	}
 
 	
-	
+	// parse the response
 	$dom->loadHTML($resp,LIBXML_NOERROR);
 	
+	// get anchors
+	// TODO add img src, video src, etc.
 	$anchors=$dom->getElementsByTagName('a');
 	
 	
 	
+	// iterate over the anchors
 	foreach($anchors as $a)
 	{
+		// finditData will be data in the crawl object at the very end
 		$finditData='';
 		
+		// the actual href value
 		$link=$a->getAttribute('href');
-		//echo "$link<br>";
+		
+		
 		if($link === '.'){
+			// special case self reference
 			$urlArr=explode('/',$url);
 			array_pop($urlArr);
 			$newUrl=join('/',$urlArr);
 			$newHomeUrl=$homeUrl;
-			//echo($newUrl);
 		}else if(preg_match('#^/#',$link)){
+			// absolute
 			$top=SchemeAndHost($url);
 			$newUrl=$top.$link;
 			$newHomeUrl=$homeUrl;
 		}else if(preg_match('#^(http|https)://#',$link)){
+			// complete uri
 			$newUrl=$link;
 			$newHomeUrl=SchemeAndHost($link);
 			
 		}else if(preg_match('#^\.\.#',$link)){
+			// relative parent folder 
 			$linkArr=explode('/',$link);
 			
 			$tempUrlOb=parse_url($url);
@@ -164,6 +219,8 @@ function NextCrawl(){
 				array_shift($linkArr);
 				array_pop($urlArr);
 			}
+			
+			// if not urlArr, something funky happened, record the error
 			if(!$urlArr){
 				$newUrl="RELATIVE ERROR";
 				$finditData=$link;
@@ -171,6 +228,7 @@ function NextCrawl(){
 				$newUrl=$tempUrlOb['scheme'].'://'.join('/',$urlArr).'/'.join('/',$linkArr);
 			}
 		}else{
+			// regular relative url
 			if($url[-1] !== '/'){$link='/'.$link;}
 			$newUrl=$url.$link;
 			$newHomeUrl=$homeUrl;
@@ -180,6 +238,8 @@ function NextCrawl(){
 		
 		$resultOb['links'][]=$newUrl;
 		
+		
+		// if the noFollow flag isn't set and the new url hasn't already been crawled, add it to the crawl arr.
 		if(!$noFollow && !array_filter($results,function($r) use ($newUrl){return $r['href'] === $newUrl;})){
 			$crawlArr[]=[
 				'href'=>$newUrl,
@@ -189,6 +249,8 @@ function NextCrawl(){
 			];
 		}
 	}
+	
+	// recursively call the next crawl
 	NextCrawl();
 	 
 	
