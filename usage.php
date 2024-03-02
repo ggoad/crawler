04fae841395302e2feab69e70a1f9084f005ced3
Greg Goad <ggoadmusic@gmail.com>
2024-03-02 16:00:44 -0500

    Fixed an error in the suggested log location- changed from .com to .txt

--- usage.php
+++ usage.php
@@ -4,7 +4,9 @@ require_once("crawlerClass.php");
 $crawler=new WAAWebCrawler\Crawler($sqlConn, $crawlPk);
 WAAWebCrawler\Crawler::$maxCrawls=4;
 WAAWebCrawler\Crawler::$userAgent="YourUA";
-WAAWebCrawler\Crawler::$logLocation="crawlerDebug.com";
+WAAWebCrawler\Crawler::$logLocation="crawlerDebug.txt";
 
+// $results is of the type WAAWebCrawler\ArrayOfCrawlResults. 
+// To get a php array, call $results->getArrayCopy();
 $results=$crawler->Crawl("https://yourWebsite.com");
 ?>
