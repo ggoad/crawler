aa55072a6cfb18ce9195ad0869480391b1d57ebc
ggoad <157517273+ggoad@users.noreply.github.com>
2024-06-19 12:56:37 -0400

    Update usage.php
    Fixed a bug in usage.php having to do with undefined variables, and also cleaned up the usage of the namespace.

--- usage.php
+++ usage.php
@@ -1,12 +1,30 @@
 <?php 
 require_once("crawlerClass.php");
 
-$crawler=new WAAWebCrawler\Crawler($sqlConn, $crawlPk);
-WAAWebCrawler\Crawler::$maxCrawls=4;
-WAAWebCrawler\Crawler::$userAgent="YourUA";
-WAAWebCrawler\Crawler::$logLocation="crawlerDebug.txt";
+use WAAWebCrawler\Crawler;
+
+Crawler::$maxCrawls=4;
+Crawler::$userAgent="YourUA";
+Crawler::$logLocation="crawlerDebug.txt";
+
+$crawler=new Crawler();
+
 
 // $results is of the type WAAWebCrawler\ArrayOfCrawlResults. 
+$results=$crawler->Crawl("https://greggoad.com");
+
 // To get a php array, call $results->getArrayCopy();
-$results=$crawler->Crawl("https://yourWebsite.com");
+$arr=$results->getArrayCopy();
+
+// echo results
+foreach($arr as $a)
+{
+	echo "{<br>";
+	foreach($a as $k=>$v)
+	{
+		echo '&nbsp;"'.$k.'":';
+		echo json_encode($v).'<br><br>';
+	}
+	echo "<br>}<br><br>";
+}
 ?>
